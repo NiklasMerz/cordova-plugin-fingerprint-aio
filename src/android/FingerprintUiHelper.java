8cd5ee01e93c924a13842856cc36bf3ae1d94aff
Niklas Merz <NiklasMerz@gmx.net>
2016-09-01 19:20:58 +0200

    Modification Notice

--- src/android/FingerprintUiHelper.java
+++ src/android/FingerprintUiHelper.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
+ * Modifications copyright (C) 2016 Niklas Merz
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
