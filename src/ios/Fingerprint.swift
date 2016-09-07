c820e293e86677451dbfb23d618a857c0dc34666
Niklas Merz <NiklasMerz@gmx.net>
2016-09-07 12:51:23 +0200

    Swift start

--- src/ios/Fingerprint.swift
+++ src/ios/Fingerprint.swift
@@ -1,4 +1,5 @@
 import Foundation
+import LocalAuthentication
 
 @objc(Fingerprint) class Fingerprint : CDVPlugin {
   func sayHello(command: CDVInvokedUrlCommand) {
@@ -9,10 +10,46 @@ import Foundation
   }
 
   func isAvailable(command: CDVInvokedUrlCommand){
+    var authenticationContext = LAContext();
+    var error:NSError?
 
+    var available = authenticationContext.canEvaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, error: &error);
+
+    var statusCode = CDVCommandStatus_ERROR;
+    var message = "isAvailable?"
+    if(available){
+      statusCode = CDVCommandStatus_OK;
+    }
+
+    let pluginResult = CDVPluginResult(status: statusCode, messageAsString: message);
+    commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId);
   }
 
   func authenticate(command: CDVInvokedUrlCommand){
-    
+    //TODO class
+    var authenticationContext = LAContext();
+
+    authenticationContext.evaluatePolicy(
+      .DeviceOwnerAuthenticationWithBiometrics,
+      localizedReason: "Only awesome people are allowed",
+      reply: { [unowned self] (success, error) -> Void in
+
+      var statusCode = CDVCommandStatus_ERROR;
+      var message = "auth?"
+      if( success ) {
+
+          // Fingerprint recognized
+          // Go to view controller
+          self.navigateToAuthenticatedViewController()
+          statusCode = CDVCommandStatus_OK;
+      }else {
+          // Check if there is an error
+          if let error = error {
+              let message = error.code
+          }
+      }
+    })
+    let pluginResult = CDVPluginResult(status: statusCode, messageAsString: message);
+    commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId);
   }
 }
