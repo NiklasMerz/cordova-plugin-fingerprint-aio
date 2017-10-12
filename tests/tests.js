02dd4983617ad86f95c7a091abdb72b2bf8bd6b8
Niklas <nm4500@rhoen.de>
2017-10-12 22:26:58 +0200

    Add tests for android and android versions

--- tests/tests.js
+++ tests/tests.js
@@ -1,32 +1,84 @@
-/* global Fingerprint */
+/* global Fingerprint cordova device */
 /* eslint-disable no-alert, no-console */
 
-exports.defineAutoTests = function() {
+exports.defineAutoTests = function () {
+  var handlers;
+
   describe("Fingerprint Object", function () {
-    it("should exist", function() {
+    it("should exist", function () {
       expect(window.Fingerprint).toBeDefined();
     });
   });
 
+  if (cordova.platformId === "android" && parseFloat(device.version) <= 5.1) {
+    describe("Android platform not supported", function () {
+      beforeEach(function () {
+        handlers = {
+          successHandler: function () { },
+          errorHandler: function () { }
+        };
+      });
+
+      it("should call the error handler when attempting to use the plugin on Android 5.1 or below", function (done) {
+        spyOn(handlers, "errorHandler").and.callFake(function (res) {
+          expect(res).toEqual(jasmine.any(Error));
+          expect(handlers.successHandler).not.toHaveBeenCalled();
+          done();
+        });
+        spyOn(handlers, "successHandler");
+
+        Fingerprint.show({
+          clientId: "Fingerprint-Tests",
+          clientSecret: "password",
+          disableBackup: true
+        }, handlers.successHandler, handlers.errorHandler);
+      });
+    });
+    return; // skip all other tests
+  }
+
+  beforeEach(function () {
+    handlers = {
+      successHandler: function () { },
+      errorHandler: function () { }
+    };
+  });
+
   describe("isAvailable", function () {
-    it("isAvailable schould be defined", function () {
+    it("schould be defined", function () {
       expect(window.Fingerprint.isAvailable).toBeDefined();
     });
 
-    it("isAvailable schould return an result or error in callback", function (done) {
-      window.Fingerprint.isAvailable( function (result) {
-        expect(result).toBeDefined();
-        done();
-      }, function(result) {
-        expect(result).toBeDefined();
+    it("schould return an result or error in callback", function (done) {
+      spyOn(handlers, "successHandler").and.callFake(function (res) {
+        console.log(res);
+        expect(res).toBeDefined();
         done();
       });
+
+      spyOn(handlers, "errorHandler").and.callFake(function (res) {
+        console.log(res);
+        expect(res).toBeDefined();
+        done();
+      });
+
+      Fingerprint.isAvailable(handlers.successHandler, handlers.errorHandler);
     });
   });
+
 
   describe("show", function () {
     it("show schould be defined", function () {
       expect(window.Fingerprint.show).toBeDefined();
+    });
+
+    it("should be able call with error or success", function () {
+      //UI interaction needed for dialog
+      Fingerprint.show({
+        clientId: "Fingerprint-Tests",
+        clientSecret: "password",
+        disableBackup: false
+      }, handlers.successHandler, handlers.errorHandler);
     });
   });
 };
