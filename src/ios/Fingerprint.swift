0276d6533dbd8bbe58afe7d9e37810e02dea2527
Jonas-Taha El Sesiy <elsesiy@gmail.com>
2017-11-04 14:55:05 +0100

    Remove duplicate platform availability check, proper code indent

--- src/ios/Fingerprint.swift
+++ src/ios/Fingerprint.swift
@@ -2,72 +2,72 @@ import Foundation
 import LocalAuthentication
 
 @objc(Fingerprint) class Fingerprint : CDVPlugin {
-
-  @objc func isAvailable(_ command: CDVInvokedUrlCommand){
-    let authenticationContext = LAContext();
-    var error:NSError?;
-    var policy:LAPolicy;
-
-    if #available(iOS 9.0, *) {
-        policy = .deviceOwnerAuthentication;
-    } else {
-        policy = .deviceOwnerAuthenticationWithBiometrics;
-    }
-
-    let available = authenticationContext.canEvaluatePolicy(policy, error: &error);
-
-    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Not available");
-    if available == true {
-      pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Available");
-    }
-
-    commandDelegate.send(pluginResult, callbackId:command.callbackId);
-  }
-
-  @objc func authenticate(_ command: CDVInvokedUrlCommand){
-    let authenticationContext = LAContext();
-    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Something went wrong");
-    var reason = "Authentication";
-    let data  = command.arguments[0] as AnyObject?;
-    var policy:LAPolicy;
-
-    if #available(iOS 9.0, *) {
-        policy = .deviceOwnerAuthentication;
-    } else {
-        policy = .deviceOwnerAuthenticationWithBiometrics;
-    }
-
-    if let disableBackup = data?["disableBackup"] as! Bool? {
-        if disableBackup {
-            authenticationContext.localizedFallbackTitle = "";
-            policy = .deviceOwnerAuthenticationWithBiometrics;
-        } else {
-          if let localizedFallbackTitle = data?["localizedFallbackTitle"] as! String? {
-            authenticationContext.localizedFallbackTitle = localizedFallbackTitle;
-          }
-      }
-    }
-
-    //Localized reason
-    if let localizedReason = data?["localizedReason"] as! String? {
-      reason = localizedReason;
-    }else if let clientId = data?["clientId"] as! String? {
-      reason = clientId;
-    }
-
-    authenticationContext.evaluatePolicy(
-      policy,
-      localizedReason: reason,
-      reply: { [unowned self] (success, error) -> Void in
-        if( success ) {
-          pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Success");
-        }else {
-          // Check if there is an error
-          if error != nil {
-            pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Error: \(String(describing:error))");
-          }
+    
+    fileprivate var policy:LAPolicy!
+    
+    func isAvailable(_ command: CDVInvokedUrlCommand){
+        let authenticationContext = LAContext();
+        var error:NSError?;
+        
+        let available = authenticationContext.canEvaluatePolicy(policy, error: &error);
+        
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Not available");
+        if available == true {
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Available");
         }
-        self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
-      });
-  }
+        
+        commandDelegate.send(pluginResult, callbackId:command.callbackId);
+    }
+    
+    func authenticate(_ command: CDVInvokedUrlCommand){
+        let authenticationContext = LAContext();
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Something went wrong");
+        var reason = "Authentication";
+        let data  = command.arguments[0] as AnyObject?;
+        
+        if let disableBackup = data?["disableBackup"] as! Bool? {
+            if disableBackup {
+                authenticationContext.localizedFallbackTitle = "";
+                policy = .deviceOwnerAuthenticationWithBiometrics;
+            } else {
+                if let localizedFallbackTitle = data?["localizedFallbackTitle"] as! String? {
+                    authenticationContext.localizedFallbackTitle = localizedFallbackTitle;
+                }
+            }
+        }
+        
+        // Localized reason
+        if let localizedReason = data?["localizedReason"] as! String? {
+            reason = localizedReason;
+        }else if let clientId = data?["clientId"] as! String? {
+            reason = clientId;
+        }
+        
+        authenticationContext.evaluatePolicy(
+            policy,
+            localizedReason: reason,
+            reply: { [unowned self] (success, error) -> Void in
+                if( success ) {
+                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Success");
+                }else {
+                    // Check if there is an error
+                    if error != nil {
+                        pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Error: \(String(describing: error?.localizedDescription))")
+                    }
+                }
+                self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
+        });
+    }
+    
+    override func pluginInitialize() {
+        super.pluginInitialize()
+        
+        guard #available(iOS 9.0, *) else {
+            policy = .deviceOwnerAuthenticationWithBiometrics
+            return
+        }
+        
+        policy = .deviceOwnerAuthentication
+    }
 }
+
