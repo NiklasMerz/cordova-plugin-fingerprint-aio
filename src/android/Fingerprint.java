12ba82ff11604d4363283bfb6735f588d147aca9
Ralph Theart <ralphtheart@urassociation.biz>
2019-08-09 17:39:43 -0400

    - using lib for prompt proved to be far better then trying to keep some old things - removed all res/ references

--- src/android/Fingerprint.java
+++ src/android/Fingerprint.java
@@ -29,18 +29,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
+import android.content.Intent;
+import android.content.Context;
+import android.app.KeyguardManager;
 
 import com.an.biometric.BiometricManager;
 import com.an.biometric.BiometricCallback;
-
+import android.hardware.biometrics.BiometricPrompt;
 
 public class Fingerprint extends CordovaPlugin implements BiometricCallback {
 	
     public static final String TAG = "Fingerprint";
     public static String packageName;
+    public static Context mContext;
     public static CallbackContext mCallbackContext;
     public static PluginResult mPluginResult;
+    private static boolean mDisableBackup = false;
+    public KeyguardManager mKeyguardManager;
     BiometricManager mBiometricManager;
+    
+    private static final int REQUEST_CODE_CONFIRM_DEVICE_CREDENTIALS = 1;
 
 	
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
@@ -48,6 +56,8 @@ public class Fingerprint extends CordovaPlugin implements BiometricCallback {
         Log.v(TAG, "Init Fingerprint");
         packageName = cordova.getActivity().getApplicationContext().getPackageName();
         mPluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        mContext = cordova.getActivity().getApplicationContext();
+        mKeyguardManager = cordova.getActivity().getSystemService(KeyguardManager.class);
     }
     
     public boolean execute(final String action,
@@ -59,23 +69,65 @@ public class Fingerprint extends CordovaPlugin implements BiometricCallback {
         final JSONObject arg_object = args.getJSONObject(0);
 
         if (action.equals("authenticate")) {
+	        
             cordova.getActivity().runOnUiThread(new Runnable() {
 	            public void run() {
-					  mBiometricManager = new BiometricManager.BiometricBuilder(cordova.getActivity().getApplicationContext())
-                        .setTitle("Title")
-                        .setSubtitle("Subtitle")
-                        .setDescription("Description")
-                        .setNegativeButtonText("Cancel")
-                        .build();
+		            
+		            if (arg_object.has("disableBackup")) {
+			            try {
+							mDisableBackup = arg_object.getBoolean("disableBackup");
+			            } catch (JSONException e) {
+				            
+			            }
+		            }
 
-		                //start authentication
-		                mBiometricManager.authenticate(Fingerprint.this);
+					//set up the builder
+					mBiometricManager = new BiometricManager.BiometricBuilder(mContext)
+						.setTitle("Title")
+						.setSubtitle("Subtitle")
+						.setDescription("Description")
+						.setNegativeButtonText("Cancel")
+						.build();
+					
+					//start authentication
+					mBiometricManager.authenticate(Fingerprint.this);
 	            }
 	        });
+	        
 	        mPluginResult.setKeepCallback(true);
             return true;
+        }else if(action.equals("isAvailable")){
+	        showAuthenticationScreen();
+	        return true;
         }
         return false;
+    }
+    
+    public void showAuthenticationScreen() {
+	    if(mKeyguardManager.isKeyguardSecure()){
+	        Intent intent = mKeyguardManager.createConfirmDeviceCredentialIntent(null, null);
+	        if (intent != null) {
+	          cordova.setActivityResultCallback(this);
+	          cordova.getActivity().startActivityForResult(intent, REQUEST_CODE_CONFIRM_DEVICE_CREDENTIALS);
+	        }
+        }else{
+	         // Show a message that the user hasn't set up a lock screen.
+	         onBiometricAuthenticationInternalError("Go to 'Settings -> Security -> Screenlock' to set up a lock screen");
+        }
+    }
+    
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    //onBiometricAuthenticationInternalError("requestCode: " + requestCode);
+	    Log.d(TAG, "onActivityResult");
+        if (requestCode == REQUEST_CODE_CONFIRM_DEVICE_CREDENTIALS) {
+            if (resultCode == cordova.getActivity().RESULT_OK) {
+              onAuthenticationSuccessful();
+            } else {
+              onBiometricAuthenticationInternalError("Pin/pattern dismissed.");
+            }
+        }
     }
     
     @Override
@@ -100,7 +152,7 @@ public class Fingerprint extends CordovaPlugin implements BiometricCallback {
 
     @Override
     public void onBiometricAuthenticationInternalError(String error) {
-        mCallbackContext.error("error");
+        mCallbackContext.error("InternalError" + error);
     }
 
     @Override
@@ -110,8 +162,11 @@ public class Fingerprint extends CordovaPlugin implements BiometricCallback {
 
     @Override
     public void onAuthenticationCancelled() {
-        mCallbackContext.error("biometric_cancelled");
+        //mCallbackContext.error("biometric_cancelled");
         mBiometricManager.cancelAuthentication();
+        if(!mDisableBackup){
+	        showAuthenticationScreen();
+        }
     }
 
     @Override
@@ -121,12 +176,24 @@ public class Fingerprint extends CordovaPlugin implements BiometricCallback {
 
     @Override
     public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
-		mCallbackContext.error(helpString + " | code: " + helpCode);
+		mCallbackContext.error(helpString + " | helpCode: " + helpCode);
     }
 
     @Override
     public void onAuthenticationError(int errorCode, CharSequence errString) {
-		mCallbackContext.error(errString + " | code: " + errorCode);
+		mCallbackContext.error(errString + " | errorCode: " + errorCode);
+		if (errorCode == 9) {
+        	if(!mDisableBackup){
+	        	mPluginResult.setKeepCallback(false);
+	        	showAuthenticationScreen();
+        	}
+	    }
     }
+    
+/*
+    public void onAuthenticationActivityFinished(){
+	    mCallbackContext.error("biometric_activity_finished");
+    }
+*/
 	
 }
