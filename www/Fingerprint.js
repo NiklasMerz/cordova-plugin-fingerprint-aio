cb57b11c971bb3e2acc97d3594a60b2a3ca897bf
Niklas Merz <niklas@merz.dev>
2020-08-11 19:58:47 +0200

    Fix parameters and add tests

--- www/Fingerprint.js
+++ www/Fingerprint.js
@@ -33,17 +33,13 @@ Fingerprint.prototype.show = function (params, successCallback, errorCallback) {
   );
 };
 
-Fingerprint.prototype.isAvailable = function (arg0, arg1, arg2) {
-  const hasParams = typeof arg0 === 'object';
-  const params = hasParams ? arg0 : {};
-  const successCallback = hasParams ? arg1 : arg0;
-  const errorCallback = hasParams ? arg2 : arg1;
+Fingerprint.prototype.isAvailable = function (successCallback, errorCallback, optionalParams) {
   cordova.exec(
     successCallback,
     errorCallback,
     "Fingerprint",
     "isAvailable",
-    [params]
+    [optionalParams]
   );
 };
 
