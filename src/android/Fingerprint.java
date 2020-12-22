582ffa9104b39f9b7ca42e1a59115f63537c17d7
DementCore <53424423+dementcore@users.noreply.github.com>
2020-12-22 10:36:34 +0100

    Update Fingerprint.java

--- src/android/Fingerprint.java
+++ src/android/Fingerprint.java
@@ -169,7 +169,7 @@ public class Fingerprint extends CordovaPlugin {
     }
 
     private void sendSuccess(String message) {
-        Log.e(TAG, message);
+        //Log.e(TAG, message);
         cordova.getActivity().runOnUiThread(() ->
                 this.mCallbackContext.success(message));
     }
